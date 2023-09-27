10aeacdf1f346a9b95ee29e63edeb9bf8d140a9b
Kacper K <69986656+Someguy211@users.noreply.github.com>
2023-09-26 19:29:42 -0500

    Update webscrape.py
    Converted all prices to strings and printed out the URL with the lowest price of humalog insulin

--- webscrape.py
+++ webscrape.py
@@ -7,40 +7,50 @@ headers =  {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 page = requests.get(url, headers = headers)
 response = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-price = soup.find(id = "total_price").get_text()
+price = float(soup.find(id="total_price").get_text().strip('$'))
 print ("$",price)
 
 
-urltwopricetwo = "https://www.google.com/shopping/product/15737249068825768559?sca_esv=568362559&rlz=1C1RXMK_enUS1019US1019&sxsrf=AM9HkKnMowcYgahQP1MSqbnGotANaqMYUw:1695693401253&q=humalog+insulin&biw=1920&bih=969&dpr=1&prds=eto:12803139871616740394_0,pid:7640690421513193980&sa=X&ved=0ahUKEwjjzsW3l8eBAxVEkokEHUjSCRkQ8wIIgAw"
-page = requests.get(urltwopricetwo, headers = headers)
-response = requests.get(urltwopricetwo)
+urltwo = "https://www.google.com/shopping/product/15737249068825768559?sca_esv=568362559&rlz=1C1RXMK_enUS1019US1019&sxsrf=AM9HkKnMowcYgahQP1MSqbnGotANaqMYUw:1695693401253&q=humalog+insulin&biw=1920&bih=969&dpr=1&prds=eto:12803139871616740394_0,pid:7640690421513193980&sa=X&ved=0ahUKEwjjzsW3l8eBAxVEkokEHUjSCRkQ8wIIgAw"
+page = requests.get(urltwo, headers = headers)
+response = requests.get(urltwo)
 soup = BeautifulSoup(page.content, 'html.parser')
-pricetwo = soup.find('span', class_='g9WBQb').get_text()
+pricetwo = float(soup.find('span', class_='g9WBQb').get_text().strip('$'))
 print (pricetwo)
 
-urlthreepricethree = "https://canadianinsulin.com/product/humalog-vial-100-units-ml/"
-page = requests.get(urlthreepricethree, headers = headers)
-response = requests.get(urlthreepricethree)
+urlthree = "https://canadianinsulin.com/product/humalog-vial-100-units-ml/"
+page = requests.get(urlthree, headers = headers)
+response = requests.get(urlthree)
 soup = BeautifulSoup(page.content, 'html.parser')
-pricethree = soup.find('p', class_='price').get_text()
+pricethree = float(soup.find('p', class_='price').get_text().strip('$'))
 print (pricethree)
 
-urlfourpricefour = "https://www.google.com/shopping/product/1?q=humalog+insulin&prds=epd:2221974124496111182,eto:2221974124496111182_0,pid:2221974124496111182&sa=X&ved=0ahUKEwjb57KgmMeBAxWVhIkEHcfxAFkQ9pwGCAU"
-page = requests.get(urlfourpricefour, headers = headers)
-response = requests.get(urlfourpricefour)
+urlfour = "https://www.google.com/shopping/product/1?q=humalog+insulin&prds=epd:2221974124496111182,eto:2221974124496111182_0,pid:2221974124496111182&sa=X&ved=0ahUKEwjb57KgmMeBAxWVhIkEHcfxAFkQ9pwGCAU"
+page = requests.get(urlfour, headers = headers)
+response = requests.get(urlfour)
 soup = BeautifulSoup(page.content, 'html.parser')
-pricefour = soup.find('span', class_='g9WBQb').get_text()
+pricefour = float(soup.find('span', class_='g9WBQb').get_text().strip('$'))
 print (pricefour) 
 
-urlfivepricefive = "https://www.google.com/shopping/product/1?q=humalog+insulin+buy+online&prds=epd:6098527067727738182,eto:6098527067727738182_0,pid:6098527067727738182&sa=X&ved=0ahUKEwj78aXLmMeBAxWJkIkEHdlMAQwQ9pwGCAU"
-page = requests.get(urlfivepricefive, headers = headers)
-response = requests.get(urlfivepricefive)
+urlfive = "https://www.google.com/shopping/product/1?q=humalog+insulin+buy+online&prds=epd:6098527067727738182,eto:6098527067727738182_0,pid:6098527067727738182&sa=X&ved=0ahUKEwj78aXLmMeBAxWJkIkEHdlMAQwQ9pwGCAU"
+page = requests.get(urlfive, headers = headers)
+response = requests.get(urlfive)
 soup = BeautifulSoup(page.content, 'html.parser')
-pricefive = soup.find('span', class_='g9WBQb').get_text()
+pricefive = float(soup.find('span', class_='g9WBQb').get_text().strip('$'))
 print (pricefive)
 
-def bestprice():
-    lowestprice = [price, pricetwo, pricethree, pricefour, pricefive]
-    lowest_value = min(lowestprice)
-    print(lowestprice)
+lowestprice = min(price, pricetwo, pricethree, pricefour, pricefive)
+print("Here is the lowest price! " + str(lowestprice))
 
+
+if(price == lowestprice):
+    print (url)
+elif(pricetwo == lowestprice):
+    print(urltwo)
+elif(pricethree == lowestprice):
+    print(urlthree)
+elif(pricefour == lowestprice):
+    print(urlfour)
+elif(pricefive == lowestprice):
+    print(urlfive)
+    
